a785a051f1e2f22a2efb753be3be198098ed5072
Dr.Kanika Singh <xboxmash2>
2026-01-28 10:58:43 +0530

    HA

--- JAVASCRIPT/Day5/script.js
+++ JAVASCRIPT/Day5/script.js
@@ -1,8 +1,14 @@
 let para="Modi ne join kari BJP fir gandi gandi chize ki desh ke bahar ja ja kar taxx ki wastage ki"
-let g=para.substring(8,para.indexOf("chize"))
+let g=para.substring(para.indexOf("BJP"),para.indexOf("chize"))
 console.log(g)
 
  let first="hypocrite"
 let last="gadkari"
 let full=(`My name is ${first} ${last}`)
 console.log(full)
+
+let arr=[1,3,5,7,9]
+let brr=[0,2,4,6,8]
+let crr=[...arr,...brr]
+console.log(crr)
+
---
+++ JAVASCRIPT/Day7/script.js
@@ -0,0 +1,33 @@
+let user = {
+    name : "Vivan",
+    Address:{
+        city:"gurugram",
+        state:"Haryana",
+    },
+    mobileno : 123456789,
+    favcolr : ["Red","green","blue"],
+    demo: function(){
+        return "demo function"
+    }
+
+}
+console.log(user)
+
+console.log(user.name,user.mobileno,user.favcolr)
+
+console.log("my name is "+user.name+" and my favourate colour is "+user.favcolr[1]);
+
+
+// string literals
+console.log(`My name is ${user.name} and my favourate colour is ${user.favcolr[0]}`);
+
+console.log(`My name is ${user.name} and my favourate colour is ${user.favcolr[2]} and i live in ${user.Address.city}`);
+console.log(user.demo())
+//object methods
+
+// syntax Object.keys(variable name)
+console.log(Object.keys(user))
+//synatx Object.values(variable name)
+console.log(Object.values(user))
+//syntax Object.values(variable name)
+console.log(Object.entries(user))
