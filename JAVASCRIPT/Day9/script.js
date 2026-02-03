1ba6433395d83ed60e7e1551e336d93d3cb50038
Dr.Kanika Singh <xboxmash2>
2026-02-03 10:08:36 +0530

    9

--- JAVASCRIPT/Day9/script.js
+++ JAVASCRIPT/Day9/script.js
@@ -1,5 +1,14 @@
 const query=document.querySelectorAll("p");
 console.log(query)
 for(i=0;i<query.length;i++){
-    query[i].textContent="afdsvf rwevfs ardfsvw"
+    if (i%2==0){
+        query[i].style.color="blue"
+    }
+    else if(i%2==0){
+        query[i].style.color="green"
+    }
+    else if(i==query.length-1)
+    {
+        query[i].style.fontWeight="bold"
+    }
 }
