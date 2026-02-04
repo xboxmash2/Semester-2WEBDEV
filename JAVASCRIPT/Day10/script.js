a56f53092a380c901b3f6cdea131f45041b3584f
Dr.Kanika Singh <xboxmash2>
2026-02-04 09:49:15 +0530

    10

---
+++ JAVASCRIPT/Day10/script.js
@@ -0,0 +1,7 @@
+const head=document.getElementById("head");
+
+console.log(head.innerText) // it will only show the visible text
+
+console.log(head.textcontent)// it will show the text including hidden text
+
+console.log(head.innerhtml)
