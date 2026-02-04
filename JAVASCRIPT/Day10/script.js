8f89b740193d98bcac32a29f84fb86b6e7a02603
Dr.Kanika Singh <xboxmash2>
2026-02-04 10:13:29 +0530

    10

--- JAVASCRIPT/Day10/script.js
+++ JAVASCRIPT/Day10/script.js
@@ -5,3 +5,13 @@ console.log(head.innerText) // it will only show the visible text
 console.log(head.textcontent)// it will show the text including hidden text
 
 console.log(head.innerhtml)
+
+// create a new element
+const newpara=document.createElement("p");
+newpara.textContent="this is a dynamically created para"
+console.log(newpara.textContent);
+
+//append the new element
+
+const container = document.getElementById("container")
+container.appendChild(newpara);
