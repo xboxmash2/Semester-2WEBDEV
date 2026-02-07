d33b9b0f7785f157a13726a69136733b160b2cf2
Dr.Kanika Singh <xboxmash2>
2026-02-08 02:36:47 +0530

    11.5

--- JAVASCRIPT/Day11/script.js
+++ JAVASCRIPT/Day11/script.js
@@ -1,10 +1,38 @@
 function HEAD(){
-    document.querySelector("#head").style.color="red";
 
+    document.querySelector("#head").style.color="red";
     document.getElementById("HEAD").textContent="color changed";
 
-    const image=document.createElement("IMG");
-    image.setAttribute("src","https://duckduckgo.com/?q=image+of+halo&ia=images&iax=images&iai=https%3A%2F%2Fwallpapertag.com%2Fwallpaper%2Ffull%2Fd%2F6%2Fe%2F602576-halo-4-wallpaper-2560x1440-2560x1600-for-windows.jpg");
+    const image=document.createElement("img");
+    image.setAttribute("src","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.RqtY5NGZFI2BRZ6aEiDj4QHaEo%3Fpid%3DApi&f=1&ipt=dbece3de5c977d54e3a7a32462e6eccd370ba987f48bc6a9f9e156bf18358168&ipo=images");
     image.setAttribute("alt","HALO");
+
     document.getElementById("head").append(image);
+
+    const l=document.createElement("a");
+    l.setAttribute("href","https://krmangalam.edu.in/");
+    l.textContent="LINK";
+
+    const box=document.createElement("div");
+    box.style.height="200px";
+    box.style.width="200px";
+    box.style.borderRadius="20px";
+    box.style.backgroundColor="tomato";
+
+    box.addEventListener("click",()=>{
+
+    
+
+        const t=document.createElement("div");
+        t.textContent="HAHAHA";
+        
+        box.style.display="flex";
+        box.style.alignItems="center";
+        box.style.justifyContent="center";
+
+        box.appendChild(t);
+    });
+
+    document.body.append(l);
+    document.body.append(box);
 }
